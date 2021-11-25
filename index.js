442adaabd86817012b6bbad91afc2dc2e1200753
omerolkun <omer.olkun@ug.bilkent.edu.tr>
2021-11-25 16:31:07 +0300

    awaits are added in front of some functions

--- index.js
+++ index.js
@@ -47,28 +47,37 @@ const opts = {
     const field_day = await client.$("id:com.example.covid19_survey_cs458:id/input_birth_date")
     const field_month = await client.$("id:com.example.covid19_survey_cs458:id/input_birth_date2")
     const field_year = await client.$("id:com.example.covid19_survey_cs458:id/input_birth_date4")
-    field_day.setValue("21")
-    field_month.setValue("08")
-    field_year.setValue("1990")
+    await field_day.setValue("21")
+    await field_month.setValue("08")
+    await field_year.setValue("1990")
     await load()
     const field_city_spinner = await client.$("id:com.example.covid19_survey_cs458:id/input_city")
 
-    field_city_spinner.click()
+    await field_city_spinner.click()
 
     const field_select_city = await client.$("xpath://*[@text='ANTALYA']")
     
   
-    field_select_city.click()
+    await field_select_city.click()
 
 
     const field_sex = await client.$("id:com.example.covid19_survey_cs458:id/input_male")
-    field_sex.click()
+    await field_sex.click()
     const field_changes = await client.$("id:com.example.covid19_survey_cs458:id/input_changes")
 
     
     field_changes.setValue("It was difficult to breath while stepping upstairs.")
     await load()
-    submit_button.click()
+    await submit_button.click()
+    await load()
+    await alert_ok.click()
+
+
+    await field_name.setValue("Ome3r olkun")
+    await submit_button.click()
+    await alert_ok.click()
+    await field_name.setValue("Omer Olkun")
+
   }
   
   main();

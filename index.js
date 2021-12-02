27937ce95ff95ee54e4858129cfb2ce317744bf3
omerolkun <omer.olkun@ug.bilkent.edu.tr>
2021-12-02 19:19:49 +0300

    something

--- index.js
+++ index.js
@@ -17,36 +17,39 @@ const opts = {
       automationName: "UiAutomator2"
     }
   };
-  
 
-  const timer = ms => new Promise(res => setTimeout(res, ms))
 
-  async function load () { // We need to wrap the loop into an async function for this to work
+const timer = ms => new Promise(res => setTimeout(res, ms))
+
+async function load () { //We need to wrap the loop into an async function for this to work
     for (var i = 0; i < 3; i++) {
-      //console.log(i);
-      await timer(3000); // then the created Promise can be awaited
+        console.log(i);
+        await timer(3000);// then the created Promise can be awaited
     }
-  }
-  
+}
 
 
 
-  async function main () {
+
+async function main () {
     const client = await wdio.remote(opts);
-  
     const field_name = await client.$("id:com.example.covid19_survey_cs458:id/input_name_surname");
-    await field_name.setValue("Omer Olkun");
-    
-    submit_button = await client.$("id:com.example.covid19_survey_cs458:id/submit_button")
-    submit_button.click()
-
-    alert_ok = await client.$("id:android:id/button3")
-    alert_ok.click()
-
-    await field_name.setValue("seyhan karaca");
     const field_day = await client.$("id:com.example.covid19_survey_cs458:id/input_birth_date")
     const field_month = await client.$("id:com.example.covid19_survey_cs458:id/input_birth_date2")
     const field_year = await client.$("id:com.example.covid19_survey_cs458:id/input_birth_date4")
+    submit_button = await client.$("id:com.example.covid19_survey_cs458:id/submit_button")
+
+    await field_name.setValue("Omer Olkun");
+    await submit_button.click()
+
+    alert_ok = await client.$("id:android:id/button3")
+    await alert_ok.click()
+    console.log("OK")
+
+    // TEST 1
+    // Everything is valid
+
+    await field_name.setValue("seyhan karaca");
     await field_day.setValue("21")
     await field_month.setValue("08")
     await field_year.setValue("1990")
@@ -55,9 +58,9 @@ const opts = {
 
     await field_city_spinner.click()
 
-    const field_select_city = await client.$("xpath://*[@text='ANTALYA']")
-    
-  
+    const field_select_city = await client.$("xpath://*[@text='ANKARA']")
+
+
     await field_select_city.click()
 
 
@@ -65,20 +68,55 @@ const opts = {
     await field_sex.click()
     const field_changes = await client.$("id:com.example.covid19_survey_cs458:id/input_changes")
 
-    
+
     field_changes.setValue("It was difficult to breath while stepping upstairs.")
     await load()
     await submit_button.click()
     await load()
     await alert_ok.click()
 
+    const mes = await client.$("id:android:id/message")
+    console.log(mes)
 
     await field_name.setValue("Ome3r olkun")
     await submit_button.click()
     await alert_ok.click()
+    await load()
     await field_name.setValue("Omer Olkun")
 
+    // // TEST 2
+    // // FEB30 WHICH IS INVALID
+    // await field_day.setValue("30")
+    // await field_month.setValue("02")
+    // await field_year.setValue("1990")
+    // await submit_button.click()
+    // await alert_ok.click()
+    // await load()
+
+    // await field_day.setValue("28")
+    // await field_month.setValue("02")
+    // await field_year.setValue("1990")
+
+    // // TEST 3
+    // await client.background(3);
+    // await submit_button.click()
+    // await alert_ok.click()
+
+    // // TEST 4
+    // // Turn Page and check wheter data is lost
+
+    // await client.setOrientation('LANDSCAPE');
+    // await client.setOrientation('PORTRAIT');
+    // console.log("OK")
+
+    // await load()
+    // await client.tapElement(submit_button)
+
+
+    // let action = new wd.TouchAction();
+    // await action.tap({el: element});
+    // await action.perform();
+
   }
-  
+
   main();
-  

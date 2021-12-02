761e8fb3d184cc63477033345e3cdfca1dba9813
omerolkun <omer.olkun@ug.bilkent.edu.tr>
2021-12-02 23:51:41 +0300

    reading alert works

--- index.js
+++ index.js
@@ -8,16 +8,34 @@ const opts = {
     path: '/wd/hub',
     port: 4723,
     capabilities: {
-      platformName: "Android",
-      platformVersion: "11",
-      deviceName: "23e0f248",
-      app: "/home/omergenc/Downloads/covid19survey.apk",
-      appPackage: "com.example.covid19_survey_cs458",
-      appActivity: ".MainActivity",
-      automationName: "UiAutomator2"
+        platformName: "Android",
+        platformVersion: "11",
+        deviceName: "23e0f248",
+        app: "/home/omergenc/Downloads/covid19survey.apk",
+        appPackage: "com.example.covid19_survey_cs458",
+        appActivity: ".MainActivity",
+        automationName: "UiAutomator2",
+        log_level: "error",
+        debug: "false",
+    },
+    appium_lib: {
+        debug: "false",
+        log_level: "error",
     }
-  };
+};
 
+async function getTextOfElement(element) {
+    let visualText;
+    try {
+        visualText = element.getText({TEXT: '*//android.widget.TextView'});
+    } catch (e) {
+        visualText = element.getText();
+    }
+    if (typeof visualText === 'string') {
+        return visualText;
+    }
+    return Array.isArray(visualText) ? visualText.join(' ') : '';
+}
 
 const timer = ms => new Promise(res => setTimeout(res, ms))
 
@@ -41,9 +59,10 @@ async function main () {
 
     await field_name.setValue("Omer Olkun");
     await submit_button.click()
-
+    await load()
     alert_ok = await client.$("id:android:id/button3")
     await alert_ok.click()
+    await load()
     console.log("OK")
 
     // TEST 1
@@ -53,36 +72,37 @@ async function main () {
     await field_day.setValue("21")
     await field_month.setValue("08")
     await field_year.setValue("1990")
+
     await load()
     const field_city_spinner = await client.$("id:com.example.covid19_survey_cs458:id/input_city")
-
     await field_city_spinner.click()
 
+    // await load()
     const field_select_city = await client.$("xpath://*[@text='ANKARA']")
-
-
     await field_select_city.click()
 
-
+    // await load()
     const field_sex = await client.$("id:com.example.covid19_survey_cs458:id/input_male")
     await field_sex.click()
+
+
     const field_changes = await client.$("id:com.example.covid19_survey_cs458:id/input_changes")
-
-
-    field_changes.setValue("It was difficult to breath while stepping upstairs.")
+    await field_changes.setValue("It was difficult to breath while stepping upstairs.")
     await load()
     await submit_button.click()
     await load()
-    await alert_ok.click()
 
     const mes = await client.$("id:android:id/message")
-    console.log(mes)
+    console.log(await getTextOfElement(mes))
 
-    await field_name.setValue("Ome3r olkun")
-    await submit_button.click()
     await alert_ok.click()
-    await load()
-    await field_name.setValue("Omer Olkun")
+
+
+    // await field_name.setValue("Ome3r olkun")
+    // await submit_button.click()
+    // await alert_ok.click()
+    // await load()
+    // await field_name.setValue("Omer Olkun")
 
     // // TEST 2
     // // FEB30 WHICH IS INVALID
